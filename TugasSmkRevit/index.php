1a3053b88a47b040ebe6ea2d32ddbb81a6928ba3
zei <129390061+sectk@users.noreply.github.com>
2024-01-16 08:10:20 +0700

    index.php

---
+++ TugasSmkRevit/index.php
@@ -0,0 +1,19 @@
+<?php
+echo "Belajar PHP";
+echo '<br>';
+echo "Saya Siswa Rpl";
+echo '<h1 style="background-color:red">Belajar PHP itu MUDAH</h1>
+    <p style="background-color:blue">Saya Suka Pemograman</p>'
+
+;
+
+echo '<br>';
+
+echo 'Saya Belajar','PHP','<br>';
+
+echo 2019 + 2;
+
+
+
+?>
+
