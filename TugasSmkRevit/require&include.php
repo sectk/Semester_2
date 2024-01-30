49b7d234e03ebd1694e4123d9bc726eed5df95ee
zei <129390061+sectk@users.noreply.github.com>
2024-01-30 09:24:33 +0700

    Create require&include.php

---
+++ TugasSmkRevit/require&include.php
@@ -0,0 +1,19 @@
+file nav :
+
+<nav>
+    <ul>
+        <li>kontak</li>
+        <li>sejarah</li>
+        <li>jurusan</li>
+    </ul>
+</nav>
+
+file php require :
+
+<?php
+
+    // require_once "nav.php";
+    // include_once "nav.php";
+
+?>
+
