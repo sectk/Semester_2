e26062a0e5cbbb6a59d99a4c949b8c9632b95c34
zei <129390061+sectk@users.noreply.github.com>
2024-02-02 10:30:16 +0700

    pelanggan.php

--- Tugas_PHP/dbtoko_forminput/pelanggan.php
+++ Tugas_PHP/dbtoko_forminput/pelanggan.php
@@ -1,13 +1,13 @@
 <form action="" method="post">
     pelanggan :
     <input type="text" name="pelanggan" placeholder="pelanggan">
-    <br>
+    <br><br>
     alamat :
     <input type="text" name ="alamat" placeholder="alamat">
-    <br>
+    <br><br>
     telepon :
     <input type="number" nama="telepon" placeholder="telepon">
-    <br>
+    <br><br>
     <input type="submit" name="simpan" value="simpan">
 </form>
 
@@ -22,13 +22,30 @@
 
     $koneksi =new mysqli($host, $user, $password, $db);
 
+    //fitur menambah barang
     if (isset($_POST["simpan"])){
-        $pelanggan = $_POST["pelanggan"];
-        $alamat = $_POST["alamat"];
-        $telepon = $_POST["telepon"];
+        $pelanggan = $_POST["pelanggan"] ?? null;
+        $alamat = $_POST["alamat"] ?? null;
+        $telepon = $_POST["telepon"] ?? null;
+
+        if(isset($_POST["id"])){
+            $id = $_POST["id"];
+        }
+       else{
         $sql = "INSERT INTO namapelanggan (pelanggan, alamat, telepon) VALUES('$pelanggan', '$alamat', $telepon)";
         $hasil = mysqli_query($koneksi, $sql);
     }
+}
+
+    //fitur menghapus
+    if(isset($_GET["hapus"])){
+        $id = $_GET["hapus"];
+        $sql = "DELETE FROM namapelanggan WHERE id= ". $id;
+        $hasil = mysqli_query($koneksi, $sql);
+
+    }
+
+
 
     $sql = "SELECT*FROM namapelanggan";
     $hasil = mysqli_query($koneksi,$sql);
@@ -48,7 +65,12 @@
 
         <th>
         STOK
+        </th>  
+
+        <th>
+        HAPUS
         </th>
+
     </tr>
     </thead>
     <tbody>";
@@ -59,6 +81,7 @@
             echo "<td>" .$row[1]."</td>";
             echo "<td>" .$row[2]."</td>";
             echo "<td>" .$row[3]."</td>";
+            echo "<td>" ."<a href ='?hapus=".$row[0]."'>hapus</a>". "</td>";
 
             echo "</tr>";
     }
