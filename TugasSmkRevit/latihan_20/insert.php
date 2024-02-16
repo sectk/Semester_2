c239341965f289942414560467c822e514924d37
zei <129390061+sectk@users.noreply.github.com>
2024-02-16 10:57:23 +0700

    Create insert.php

---
+++ TugasSmkRevit/latihan_20/insert.php
@@ -0,0 +1,28 @@
+<form action="" methdo="post">
+
+ketegori:
+<input type="text" name="kategori">
+<br>
+<input type="submit" name="simpan" value="simpan">
+
+</form>
+
+
+
+<?php 
+    require_once "../function.php";
+
+    if(isset($_POST['simpan'])){
+
+        $kategori = $_POST['kategori'];
+
+        $sql = "INSERT INTO tabelkategori VALUES ('', '$kategori');
+    
+        $result = mysqli_query($koneksi, $sql);
+    
+        header("location:http://localhost/belajarE/restoran/kategori/select.php");
+    }
+
+
+
+?>
