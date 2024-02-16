82fc4540b135f85af17d51225498a55c2fb4154a
zei <129390061+sectk@users.noreply.github.com>
2024-02-16 11:31:26 +0700

    Create select5.php

---
+++ TugasSmkRevit/latihan_22/select5.php
@@ -0,0 +1,81 @@
+<div style="margin:auto; width:900px;">
+
+<h3><a href="http://localhost/belajarE/restoran/kategori/insert.php">TAMBAH DATA</a></h3>
+
+<?php 
+
+    require_once "../funcion.php";
+
+
+
+    $sql = "SELECT idkkategori FROM tabelkategori";
+    $result = mysqli_query($koneksi, $sql);
+
+    $jumlahdata = mysqli_num_rows($result);
+
+
+    if(isset($_GET['hapus'])){
+        $id = $_GET['hapus'];
+        require_once 'delete.php';
+    }
+
+    if(isset($_GET['ubah'])){
+        $id = $_GET['ubah'];
+        require_once 'update.php';
+    }
+
+
+
+
+        echo '<br>';
+    
+    // $mulai = 3;
+    // $banyak = 3;
+
+    // $halaman = ceil ($jumlahdata / $banyak);
+ 
+    // for ($i=1; $i <=$halaman; $i++){
+    //     echo '<a href="?p=' .$i'">'.$i.'</a>';
+    //     echo '&nbsp &nbsp &nbsp';
+    // }
+
+   
+
+
+    $sql = "SELECT * FROM tabelkategori LIMIT $mulai, $banyak";
+    $result = mysqli_num_rows($koneksi, $sql);
+
+    
+    // var_dump($result);
+
+    $jumlah = mysqli_num_rows($result);
+    // echo "<br>";
+    // echo $jumlah;
+
+    echo '
+    
+    <table border="1px">
+    <tr>
+        <th>NO</th>
+        <th>Kategori</th>
+        <th>Hapus</th>
+    </tr>
+
+    ';
+
+    $no =$mulai+1;
+    if ($jumlah >0){
+        while($row = mysqli_fetch_assoc($result)){
+            echo '<tr>';
+            echo '<td>'.$no++.'</td>';
+            echo '<td>'.$row['kategori'].'/td>';
+            echo '<td><a href="?hapus='.$row['idkkategori'].'">.'.'Hapus'.'</td>';
+            echo '<td><a href="?ubah='.$row['idkkategori'].'">.'.'Ubah'.'</td>';
+            echo '<br>';
+            echo '</tr>';
+        }
+    }
+    
+    echo '</table>';
+?>
+
