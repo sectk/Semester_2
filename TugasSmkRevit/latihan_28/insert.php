c565984514c801cbefbdc97d6a82443ae953239f
zei <129390061+sectk@users.noreply.github.com>
2024-02-22 13:32:25 +0700

    Create insert.php

---
+++ TugasSmkRevit/latihan_28/insert.php
@@ -0,0 +1,32 @@
+<h3>insert kategori</h3>
+<div class="form-group">
+    <form action="" method="post"></form>
+    <div class="form-group w-50">
+        <label for="">kategori</label>
+        <input type="text" name="kategori" required placeholder="isi kategori" class="form-control">
+
+    </div>
+    <div>
+        <input type="submit" name="simpan" value="simpan">
+    </div>
+
+
+
+
+</div>
+
+<?php
+
+if(isset($POST['simpan'])){
+    $kategori = $_POST['kategori'];
+
+
+    $sql = "INSERT INTO tabelkategori VALUE ('', '$kategori')";
+    echo $sql;
+}
+
+
+
+?>
+
+
