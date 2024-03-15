3eeebb3406e7ddb71bb9f506cf1bb466e0bda409
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-15 11:18:34 +0700

    Create login.php

---
+++ TugasSmkRevit/latihan_34_tampilanuser/admin/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login restoran</title>
+</head>
+<body>
+    <div class="container">
+
+    <div class="row">
+        <div class="col-4 mx-auto mt-4">
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="">EMAIL</label>
+                    <input type="email" name="email" required placeholder="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">PAASWORD</label>
+                    <input type="email" name="password" required placeholder="password" class="form-control">
+                </div>
+
+                <div>
+                    <input type="submit" name="login" value="LOGIN" class="btn btn-primary">
+                </div>
+            </form>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
