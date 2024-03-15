6cdfd14e52ed075556eabfc3c0fa79b7035ca8a2
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-15 11:18:55 +0700

    Create index.php

---
+++ TugasSmkRevit/latihan_34_tampilanuser/admin/index.php
@@ -0,0 +1,58 @@
+<?php
+
+    session_start();
+    require_once '../dbcontroller.php';
+    $db = new DB;
+
+    if(isset($_SESSION['user'])){
+        header("location:login.php");
+    }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page | Aplikasi Restoran SMK </title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+
+   <div class="row">
+    <div class="col-md-3">
+        <h2>Restoran</h2>
+    </div>
+
+    <div class="col-md-9">
+        <div class="float-right mt-4"> logout</div>
+        <div class="float-right mt-4 mr-4"> user</div>
+        
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+            <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="?f=kategori&m=select">kategori</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=menu&m=select">menu</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=pelanggan&m=select">pelanggan</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=order&m=select">order</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=orderddetail&m=select">order detail</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=user&m=select">user</a></li>
+            </ul>
+        </div>
+   
+    </div>
+
+    </div>
+
+         <div class="row mt-5">
+                <div class="col">
+                    <p class="text-center">2024 - copyright@smkrevit.com</p>
+                </div>
+         </div>
+   
+</body>
+</html>
